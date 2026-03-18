eb26d59722fe233c8aab112a796e51874e962bf1
Rustenburg64 <aadf.aa1d@gmail.com>
2026-03-19 01:57:04 +0900

    Add diffview mappings for local and main diffs

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -3,6 +3,8 @@ return {
   cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
   keys = {
     { '<leader>do', '<cmd>DiffviewOpen<cr>', desc = '[d]iffview [O]pen' },
+    { '<leader>dm', '<cmd>DiffviewOpen<cr>', desc = '[d]iffview [M]odified files' },
+    { '<leader>dM', '<cmd>DiffviewOpen main...HEAD --imply-local<cr>', desc = '[d]iffview [M]ain diff' },
     { '<leader>dh', '<cmd>DiffviewFileHistory %<cr>', desc = '[d]iffview file [H]istory' },
     { '<leader>dH', '<cmd>DiffviewFileHistory<cr>', desc = '[d]iffview repo [H]istory' },
     { '<leader>dq', '<cmd>DiffviewClose<cr>', desc = '[d]iffview [Q]uit' },
